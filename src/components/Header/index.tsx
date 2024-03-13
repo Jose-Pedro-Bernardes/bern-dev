0642d93e83c52c2c713ddbe4283d9cd52da435dc
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-12 22:18:22 -0300

    Feat(Header mobile): esqueleto do header versão mobile (html)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header>Header</header>
+    <header>
+      <figure>
+        <Image src="/images/logo/logoHeader.svg" alt="Logotipo da marca escrito BN" width={50} height={41} />
+      </figure>
+      <button>
+        <Image src="/images/assets/menu.svg" alt="" width={34} height={34} />
+      </button>
+    </header>
   )
 }
