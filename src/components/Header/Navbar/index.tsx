9f2eb04af1e6af538e5d5f48186c0852d6b1e003
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-12 22:33:13 -0300

    feat(Header Mobile): Definir parte do esqueleto do componente Navbar

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav>Navbar</nav>
+    <nav>
+      <Link href="home">Home</Link>
+      <Link href="sobre">Sobre</Link>
+      <Link href="contatos">Contatos</Link>
+      <Link href="#Redes">Redes</Link>
+    </nav>
   )
 }
