91df321669572cc54db7a65cfd98643cdcbbd8cf
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-12 22:27:38 -0300

    Feat(Footer Mobile): Definir esqueleto do footer versão mobile (HTML)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <section>
+        <figure>
+          <Image src="/images/logo/logoFooter" alt="" width={50} height={41} />
+        </figure>
+        <p>José Pedro Bernardes.</p>
+        <p>josepedro.bernardes1@gmail.com</p>
+      </section>
+      <section>
+        <h4>Redes Sociais</h4>
+        <nav>
+          <a href="#" target="_blank">{/*Instagram*/}</a>
+          <a href="#" target="_blank">{/*LinkedIn*/}</a>
+          <a href="#" target="_blank">{/*GitHub*/}</a>
+          <a href="#" target="_blank">{/*WhatsApp*/}</a>
+        </nav>
+      </section>
+    </footer>
   )
 }
