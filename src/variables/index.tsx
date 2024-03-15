1486ec089aac32f0d89f7f959b75f828a57a333a
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-15 19:28:03 -0300

    arquiteture: arquivo "variables.ts" pra guardar strings com um tamanho grande.

---
+++ src/variables/index.tsx
@@ -0,0 +1,5 @@
+const DESCRIPTION = `Como desenvolvedor web, tenho a responsabilidade de criar e
+gerenciar sites únicos, oferecendo soluções personalizadas para uma variedade de
+necessidades.`
+
+export { DESCRIPTION }
