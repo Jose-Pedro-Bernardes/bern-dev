06255ad8c7bcc698773d805e6424604e57a99b0e
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-15 19:33:05 -0300

    Feat: Criar lógica para header mudar a cor quando sair da posição "0".

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,42 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
+  const [isAtTop, setIsAtTop] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop
+      setIsAtTop(scrollTop === 0)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header>
-      <figure>
-        <Image src="/images/logo/logoHeader.svg" alt="Logotipo da marca escrito BN" width={50} height={41} />
-      </figure>
-      <button>
-        <Image src="/images/assets/menu.svg" alt="" width={34} height={34} />
-      </button>
+    <header
+      className={`fixed top-0 left-0 w-full z-10 transition-all duration-300 ease-in-out ${
+        isAtTop ? 'bg-transparent text-white' : 'bg-white text-black'
+      }`}
+    >
+      <nav className="container mx-auto flex justify-between items-center py-4">
+        <figure>
+          <Image
+            src="/images/logo/logoHeader.svg"
+            alt="Logotipo da marca escrito BN"
+            width={50}
+            height={41}
+          />
+        </figure>
+        <button>
+          <Image src="/images/assets/menu.svg" alt="" width={34} height={34} />
+        </button>
+      </nav>
     </header>
   )
 }
