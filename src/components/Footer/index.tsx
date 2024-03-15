c45effe3eca3ffe74df021dc803bd94fdca4945d
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-15 19:32:22 -0300

    small: Estruturando esqueleto do footer conforme a configuração do eslint x prettier.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Footer() {
   return (
     <footer id="Redes">
       <section>
         <figure>
-          <Image src="/images/logo/logoFooter" alt="" width={50} height={41} />
+          <Image
+            src="/images/logo/logoFooter.svg"
+            alt=""
+            width={50}
+            height={41}
+          />
         </figure>
         <p>José Pedro Bernardes.</p>
         <p>josepedro.bernardes1@gmail.com</p>
@@ -13,10 +18,18 @@ export default function Footer() {
       <section>
         <h4>Redes Sociais</h4>
         <nav>
-          <a href="#" target="_blank">{/*Instagram*/}</a>
-          <a href="#" target="_blank">{/*LinkedIn*/}</a>
-          <a href="#" target="_blank">{/*GitHub*/}</a>
-          <a href="#" target="_blank">{/*WhatsApp*/}</a>
+          <a href="#" target="_blank">
+            {/* Instagram */}
+          </a>
+          <a href="#" target="_blank">
+            {/* LinkedIn */}
+          </a>
+          <a href="#" target="_blank">
+            {/* GitHub */}
+          </a>
+          <a href="#" target="_blank">
+            {/* WhatsApp */}
+          </a>
         </nav>
       </section>
     </footer>
