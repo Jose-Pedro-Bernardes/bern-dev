e63a99b9afb6f7e4bb470a9cb760a71616ec0ff5
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-19 19:01:09 -0300

    Feat(Navbar mobile): Implementar menu de navegação mobile.

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -9,34 +9,83 @@ interface IPROP {
 
 export default function Navbar({ setMenuOpen }: IPROP) {
   return (
-    <>
-      <header>
-        <figure>
+    <nav className="mb-[40px]">
+      <header className="flex items-center justify-between px-[30px] bg-white h-[101px]">
+        <a href="/" target="_self">
+          <figure>
+            <Image
+              src="/images/logo/bn.svg"
+              alt="Logotipo da marca escrito BN"
+              width={45}
+              height={41}
+              layout="responsive"
+              className="invert-[.80]"
+            />
+          </figure>
+        </a>
+        <button onClick={() => toggleBooleanState(setMenuOpen)}>
           <Image
-            src="/images/logo/bn.svg"
-            alt="Logotipo da marca escrito BN"
-            width={50}
-            height={41}
-            className="invert-[.80]"
+            src="/images/assets/x.svg"
+            alt="Botão de fechar menu de navegação."
+            width={34}
+            height={34}
+            className="invert-[.10]"
           />
-        </figure>
-        <Image
-          src="/images/logo/bn.svg"
-          alt="Logotipo da marca escrito BN"
-          width={50}
-          height={41}
-          className="invert-[.80]"
-        />
-        <figure></figure>
+        </button>
       </header>
-      <nav>
-        <Link href="home">Home</Link>
-        <Link href="sobre">Sobre</Link>
-        <Link href="contatos">Contatos</Link>
-        <Link onClick={() => toggleBooleanState(setMenuOpen)} href="#Redes">
-          Redes
-        </Link>
+      <nav className="flex flex-col bg-[#EEEEEE]">
+        <div className="flex gap-5 items-center box-border pl-[30px] h-[96px] border-solid border-b-[1px] border-[#B5B5B5]">
+          <Image
+            src="/images/assets/navbar/home.svg"
+            alt=""
+            width={25}
+            height={25}
+            className="invert-[.10]"
+          />
+          <Link href="home">Home</Link>
+        </div>
+        <div className="flex gap-5 items-center box-border pl-[30px] h-[96px] border-solid border-b-[1px] border-[#B5B5B5]">
+          <Image
+            src="/images/assets/navbar/sobre.svg"
+            alt=""
+            width={25}
+            height={25}
+            className="invert-[.10]"
+          />
+          <Link href="sobre">Sobre</Link>
+        </div>
+        <div className="flex gap-5 items-center box-border pl-[30px] h-[96px] border-solid border-b-[1px] border-[#B5B5B5]">
+          <Image
+            src="/images/assets/navbar/contatos.svg"
+            alt=""
+            width={25}
+            height={25}
+            className="invert-[.10]"
+          />
+          <Link href="contatos">Contatos</Link>
+        </div>
+        <div className="flex gap-5 items-center box-border pl-[30px] h-[96px] border-solid border-b-[1px] border-[#B5B5B5]">
+          <Image
+            src="/images/assets/navbar/redes.svg"
+            alt=""
+            width={25}
+            height={25}
+            className="invert-[.10]"
+          />
+          <Link
+            onClick={() => toggleBooleanState(setMenuOpen)}
+            href="#Redes"
+            target="_parent"
+          >
+            Redes
+          </Link>
+        </div>
+        <div className="flex justify-center items-center h-[135px] text-[#2187BF] font-bold">
+          <button className="flex justify-center items-center w-[151px] h-[45px] bg-transparent border-solid border-[1px] border-[#2187BF] rounded-[17px]">
+            Comece hoje
+          </button>
+        </div>
       </nav>
-    </>
+    </nav>
   )
 }
