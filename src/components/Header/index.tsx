7c6ceb854ea5c84978f554c59f2fd18bf0c4e492
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-21 11:21:45 -0300

    Style: Definir estilização para header component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -26,8 +26,10 @@ export default function Header() {
     <>
       {!menuOpen ? (
         <header
-          className={`flex items-center h-24 fixed top-0 left-0 w-full z-10 px-[30px] ${
-            isAtTop ? 'bg-transparent text-white' : 'bg-white text-black'
+          className={`flex items-center h-24 fixed top-0 left-0 w-full z-10 px-[30px] transition-all duration-500 ease-in-out ${
+            isAtTop
+              ? 'bg-transparent text-white'
+              : 'bg-[rgb(255,255,255,40%)] text-[#ffffff]'
           }`}
         >
           <nav className="container mx-auto flex justify-between items-center py-4 lg:px-[6%]">
@@ -38,7 +40,6 @@ export default function Header() {
                   alt="Logotipo da marca escrito BN"
                   width={50}
                   height={41}
-                  className={!isAtTop ? 'invert-[.80]' : ''}
                 />
               </figure>
             </a>
@@ -54,19 +55,39 @@ export default function Header() {
                 className={!isAtTop ? 'invert-[.80]' : ''}
               />
             </button>
-            <nav className="hidden lg:flex h-full items-end gap-[60px] font-light text-[16px] xl:gap-[94px]">
-              <Link href={'/'}>Home</Link>
-              <Link href={'/sobre'}>Sobre</Link>
-              <Link href={'/contatos'}>Contatos</Link>
-              <Link href={'#Redes'}>Redes</Link>
+            <nav className="hidden lg:flex h-full items-end gap-[60px] font-normal  text-[0.875rem] xl:gap-[94px] xl:text-[1rem]">
+              <Link
+                className="transition-transform duration-200 ease-in-out hover:scale-125 hover:font-medium hover:text-[#96d9ff]"
+                href={'/'}
+              >
+                Home
+              </Link>
+              <Link
+                className="transition-transform duration-200 ease-in-out hover:scale-125 hover:font-medium hover:text-[#96d9ff]"
+                href={'/sobre'}
+              >
+                Sobre
+              </Link>
+              <Link
+                className="transition-transform duration-200 ease-in-out hover:scale-[1.15] hover:font-medium hover:text-[#96d9ff]"
+                href={'/contatos'}
+              >
+                Contatos
+              </Link>
+              <Link
+                className="transition-transform duration-200 ease-in-out hover:scale-[1.15] hover:font-medium hover:text-[#96d9ff]"
+                href={'#Redes'}
+              >
+                Redes
+              </Link>
             </nav>
             <a
               href="/contatos"
               target="_parent"
               className={`hidden lg:flex justify-center 
               items-center w-[151px] h-[45px] bg-transparent border-solid border-[1px]
-              font-bold rounded-[17px] 
-              ${isAtTop ? 'border-[#8CD6FF] text-[#8CD6FF]' : 'border-[#2187BF] text-[#2187BF]'}`}
+              font-bold rounded-[17px] transition-all duration-500 ease-in-out
+              ${isAtTop ? 'border-[#8CD6FF] text-[#8CD6FF] hover:bg-[rgb(0,0,0,20%)] hover:border-white hover:text-white' : 'border-white text-white bg-[#0000004a] hover:bg-[#0000009e]'}`}
             >
               Comece hoje
             </a>
