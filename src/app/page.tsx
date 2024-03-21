42d8ae9a3bfd4945ed2c06e58c1fea70ce5bc1d8
jose-pedro <josepedro.bernardes1@gmail.com>
2024-03-21 11:18:53 -0300

    Feat (Em desenvolvimento): Conteúdo da homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="mt-[200px] bg-black w-full h-[400px]">
       <h1 className="text-transparent">JOSÉ</h1>
     </main>
   )
